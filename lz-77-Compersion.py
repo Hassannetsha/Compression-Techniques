be72a4138ab861fe2430f3820ea5dc6b0838a130
Noureldin Shimi <nouredinshimi@gmail.com>
2024-10-25 03:12:27 +0300

    simple comp. works(w files)

--- lz-77-Compersion.py
+++ lz-77-Compersion.py
@@ -9,29 +9,35 @@ def lz_77_Compression(data, s_buffer_size, la_buffer_size):
 
         la_buffer = data[la_position:la_position+la_buffer_size]
         longest_match = la_buffer[0]
-        index = s_buffer.find(longest_match)
+        index = s_buffer.rfind(longest_match)
 
         while index != -1 and (la_position + length) < len(data):
             length += 1
-            position = index
-            longest_match = la_buffer[:length]
-            index = s_buffer.find(longest_match)
+            position = len(s_buffer) - index
+            longest_match = la_buffer[:length+1]
+            index = s_buffer.rfind(longest_match)
 
-        compressed_data.append( f"({position},{length},{data[la_position+length]})")
+        compressed_data.append( f"({position},{length},{longest_match[-1]})")
 
         s_buffer += data[la_position:la_position + length + 1]
         s_buffer = s_buffer[-s_buffer_size:]
         la_position += length + 1
-
-
     return compressed_data
 
 def main():
-    d = "ABAABABAABBBBBBBBBBBBA"
+    with open('baseD.txt', 'r') as file:
+        d = file.read()
+
     s = 12
     la = 11
+    # c = ['(5,10,NULL)','(5,1,23)']
     c = lz_77_Compression(d, s, la)
-    print(c)
+
+    with open('baseC.txt', 'w') as file:
+        for tag in c:
+            tag = tag.strip('()').split(",")
+
+            file.write(f'<{tag[0]},{tag[1]},{"NUll" if tag[2] == "NULL" else f'"{tag[2]}"'}>\n')
 
 
 main()
